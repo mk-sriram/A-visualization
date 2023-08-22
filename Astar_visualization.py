f9a63ea584f225901a2d0333c8e553ea70a823f9
Sriram MK <mksriram@umich.edu>
2023-08-23 02:33:00 +0300

    updates and changes, working however issues with efficiency

--- Astar_visualization.py
+++ Astar_visualization.py
@@ -49,27 +49,39 @@ class Spot:  #singular spot and its attributes
     def is_end(self):
         return self.color == PURPLE
     def reset(self):
-        self.color == WHITE
+        self.color = WHITE
 
     ##Functions to alter the values 
     def make_closed(self):
-        self.color == RED
+        self.color = RED
     def make_open(self):
-        self.color == GREEN
+        self.color = GREEN
     def make_barrier(self):
-        self.color == BLACK
+        self.color = BLACK
     def make_start(self):
-        self.color == ORANGE
+        self.color = ORANGE
     def make_end(self):
-        self.color == BLACK
+        self.color = TURQUOISE
     def make_path(self):
-        self.color == PURPLE
+        self.color = PURPLE
     #function to draw the lines
     def draw(self, win):
-        pygame.draw.rect(win,self.color,(self.x,self.y,self.width,self.width))
+        pygame.draw.rect(win, self.color,(self.x, self.y, self.width, self.width))
     
     def update_neighbors(self,grid):
-        pass
+        self.neighbors = [] 
+        if self.row < self.total_rows - 1 and not grid[self.row + 1 ][self.col].is_barrier(): #down checking if you can move down 
+            self.neighbors.append(grid[self.row + 1][self.col])
+            
+        if self.row > 0 and not grid[self.row - 1 ][self.col].is_barrier(): #down checking if you can move down 
+            self.neighbors.append(grid[self.row - 1][self.col])
+        
+        if self.col < self.total_rows - 1 and not grid[self.row ][self.col + 1].is_barrier(): #RIGHT checking if you can move down 
+            self.neighbors.append(grid[self.row][self.col + 1])
+        
+        if self.col > 0 and not grid[self.row][self.col - 1].is_barrier(): #LEFT checking if you can move down 
+            self.neighbors.append(grid[self.row][self.col - 1])
+
     def __lt__(self, other): #less than function 
         #handling if the comparing two spots together, comparing one spot to another spot
         return False
@@ -79,6 +91,58 @@ def h(p1,p2): #Heuristic Function ( manhattan distance )
     x2,y2 = p2 
 
     return abs(x1 - x2 ) + abs(y1 - y2)
+
+def path_reconstruction(came_from,current,draw):
+    while current in came_from:
+        current = came_from[current]
+        current.make_path()
+        draw()
+
+def algorithm(draw, grid, start, end):
+    count = 0
+    open_set = PriorityQueue() # efficient way to get the minimum value node associated with it 
+    open_set.put((0,count, start))
+    came_from = {}
+    g_score  = {spot: float("inf") for row in grid for spot in row} #list comprehension #current shortest disntanec from infinitey to node 
+    g_score[start] = 0
+    f_score  = {spot: float("inf") for row in grid for spot in row} #list comprehension 
+    f_score[start] = h(start.get_position(), end.get_position())
+
+    open_set_hash = {start}
+
+    while not open_set.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        current = open_set.get()[2] #pops the lowest value f score, current node we're looknig at , will be start
+        open_set_hash.remove(current) #remove it and sync it with the hash 
+
+        if current == end: 
+            path_reconstruction(came_from,end,draw)
+            end.make_end()
+            return True
+        
+        for neighbor in current.neighbors:
+            temp_gscore = g_score[current] + 1 
+
+            if temp_gscore < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = temp_gscore
+                f_score[neighbor] = temp_gscore + h(neighbor.get_position(), end.get_position()) #h function requires the spot and the end to calcualte approx distance 
+
+                if neighbor not in open_set_hash:
+                    count += 1 
+                    open_set.put((f_score[neighbor], count , neighbor))
+                    open_set_hash.add(neighbor)
+
+                    neighbor.make_open() # know that we already considered this and is a valid option
+        draw()
+ # if the node we considered is not the start node, then just close it 
+        if current != start:
+            current.make_closed()
+
+    return False
 
 def make_grid(rows,width):
     grid1 = []
@@ -156,8 +220,31 @@ def main(win, width):
                     spot.make_barrier()
 
             elif pygame.mouse.get_pressed()[2]: #right mouse button
-                pass
+                pos = pygame.mouse.get_pos()
+                row,col = get_clicked_pos(pos,ROWS,width)
+                spot = grid[row][col]
+                spot.reset()
+                if spot == start:
+                    start = None
+                if spot == end:
+                    end == None
+
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and not started: # checks 
+                    for row in grid:
+                        for spot in row:
+                            spot.update_neighbors(grid)
+
+                        algorithm(lambda: draw( win, grid, ROWS, width), grid, start, end)
+                if event.key == pygame.K_c:
+                    start = None
+                    end = None 
+                    grid = make_grid(ROWS, width)
     pygame.quit() 
 
 
 main(WIN,WIDTH)
+
+# issue 1 : the button clisk didn't wokr - the spots were not updating the colors as the assignment operator was incorrect 
+# issue 2 : green was not spot for second click - color was incorrect in the make methods 
